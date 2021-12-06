b593321f8edc23f21bac3fa2485706923fc4e537
Kaushik Sambamurthy <kaushik.s02@gmail.com>
2021-12-06 09:03:35 -0500

    Create utils directory

---
+++ utils/filehandler.cpp
@@ -0,0 +1,16 @@
+#include "filehandler.hpp"
+
+namespace utils
+{
+
+FileHandler::FileHandler(std::string filepath) {
+    stream.open(filepath);
+    if (!stream) {
+        std::cerr << "Unable to open file.\n";
+        exit(1);
+    }
+}
+
+FileHandler::~FileHandler() { stream.close(); }
+
+}
