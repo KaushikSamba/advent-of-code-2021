1a95b2a4f99a8527c3d1b5333b6ee91ed72c8298
Kaushik Sambamurthy <kaushik.s02@gmail.com>
2021-12-06 23:09:49 -0500

    Part 1 solution

--- day-1/day-1-sonar-sweep.cpp
+++ day-1/day-1-sonar-sweep.cpp
@@ -1,6 +1,26 @@
 #include "utils/filehandler.hpp"
 
-int main()
+long countIncreases(std::vector<long> inputVector)
 {
+    int numIncreases = 0;
+    for(std::size_t i = 1; i < inputVector.size(); i++)
+    {
+        const auto val = inputVector[i];
+        if(val > inputVector[i - 1])
+        {
+            numIncreases++;
+        }
+    }
+    return numIncreases;
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "Filename: " << argv[1] << '\n';
+    auto inputVector = utils::FileHandler {argv[1]}.getInputVector<long>();
+
+    auto result = countIncreases(inputVector);
+
+    std::cout << "Final answer: " << result << '\n';
     return 0;
 }
