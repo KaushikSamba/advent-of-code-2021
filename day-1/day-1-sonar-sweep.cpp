b5f091c96e89a54ea8a8fd62c63746e992f46157
Kaushik Sambamurthy <kaushik.s02@gmail.com>
2021-12-06 23:14:11 -0500

    Add part 2 solution

--- day-1/day-1-sonar-sweep.cpp
+++ day-1/day-1-sonar-sweep.cpp
@@ -19,7 +19,15 @@ int main(int argc, char** argv)
     std::cout << "Filename: " << argv[1] << '\n';
     auto inputVector = utils::FileHandler {argv[1]}.getInputVector<long>();
 
-    auto result = countIncreases(inputVector);
+    // auto result = countIncreases(inputVector);
+    // Part 2 solution
+    std::vector<long> slidingWindowVector;
+    for(std::size_t i = 2; i < inputVector.size(); i++)
+    {
+        slidingWindowVector.push_back(inputVector[i] + inputVector[i - 1] + inputVector[i - 2]);
+    }
+    auto result = countIncreases(slidingWindowVector);
+    // End part 2 solution
 
     std::cout << "Final answer: " << result << '\n';
     return 0;
