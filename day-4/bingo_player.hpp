912fed0e02578c366a8224900bde1f1e4d2826c8
Kaushik Sambamurthy <kaushik.sambamurthy@alertinnovation.com>
2021-12-14 13:59:55 -0500

    Solved part 1

--- day-4/bingo_player.hpp
+++ day-4/bingo_player.hpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <queue>
 #include <vector>
 
 namespace solution
@@ -22,13 +23,29 @@ public:
     void setSelectedNumbers(std::vector<uint> const& selectedNumbers);
 
     void parseInputFile(std::istream& stream);
+    uint play();
 
     std::vector<uint>                           getNumberList() const;
     std::vector<std::vector<std::vector<uint>>> getParsedBoards() const;
 
+    uint getNextNumber();
+
+    uint calculatePart1Answer(BOARD const& board, uint lastNumber);
+
+    static uint getSumOfUnmarkedNumbers(BOARD const& board);
+    static bool findAndMarkNumberOnBoard(BOARD& board, uint number);  // True if bingo, false otherwise.
+    static void markNumberOnBoard(BOARD& board, std::size_t row, std::size_t col);
+    static std::tuple<bool, std::size_t, std::size_t> findNumberInBoard(BOARD& board, uint number);
+    static bool                                       checkBingoRow(BOARD& board, std::size_t row);
+    static bool                                       checkBingoColumn(BOARD& board, std::size_t col);
+
+    std::vector<BOARD> m_boards;
+
 private:
+    void createInternalBoardRepresentation();
+
     std::vector<uint>                           m_number_list;
     std::vector<std::vector<std::vector<uint>>> m_parsed_boards;
-    std::vector<BOARD>                          m_boards;
+    std::queue<uint>                            m_number_queue;
 };
 }  // namespace solution
