f7cff5259726c73e7ff8f1240db505cdc3d661bc
Kaushik Sambamurthy <kaushik.sambamurthy@alertinnovation.com>
2021-12-14 10:48:05 -0500

    Parse input file in solver class

--- day-4/bingo_player.hpp
+++ day-4/bingo_player.hpp
@@ -6,8 +6,8 @@ namespace solution
 
 struct BoardElement
 {
-    uint8_t number;
-    bool    marked = false;
+    uint number;
+    bool marked = false;
 };
 
 static constexpr std::size_t BOARD_SIZE = 5;
@@ -19,10 +19,16 @@ class BingoPlayer
 public:
     BingoPlayer();
 
-    void setSelectedNumbers(std::vector<uint8_t> const& selectedNumbers);
+    void setSelectedNumbers(std::vector<uint> const& selectedNumbers);
+
+    void parseInputFile(std::istream& stream);
+
+    std::vector<uint>                           getNumberList() const;
+    std::vector<std::vector<std::vector<uint>>> getParsedBoards() const;
 
 private:
-    std::vector<uint8_t> m_number_list;
-    std::vector<BOARD>   m_boards;
+    std::vector<uint>                           m_number_list;
+    std::vector<std::vector<std::vector<uint>>> m_parsed_boards;
+    std::vector<BOARD>                          m_boards;
 };
 }  // namespace solution
